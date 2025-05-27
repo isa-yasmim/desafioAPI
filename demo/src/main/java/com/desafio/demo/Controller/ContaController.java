c64a6aa88ac696f832aada4b99e4b349a33700df
Isabela <osowskiisabela@gmail.com>
2025-05-27 10:33:37 -0300

    Adicionado um arquivo de configuracao CORS para toda a api

--- demo/src/main/java/com/desafio/demo/Controller/ContaController.java
+++ demo/src/main/java/com/desafio/demo/Controller/ContaController.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,7 +43,7 @@ public class ContaController {
         return contaService.findAll();
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
+    //@CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/{id}")
     public ResponseEntity<Conta> getConta(@PathVariable Long id) {
         return contaService.findById(id)
